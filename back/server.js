f4c03ea5a4eebe9e474613c2b6329515db7c421b
egortg <egorgorodinskij85@gmail.com>
2025-04-19 11:21:17 +0300

    3 commit

--- back/server.js
+++ back/server.js
@@ -23,10 +23,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/front/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "front", "dist", "index.html"));
 	});
 }
 
