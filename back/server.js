ea76b171370f60e509e1c1aeb069bada1874162c
egortg <egorgorodinskij85@gmail.com>
2025-04-19 11:20:37 +0300

    2 commit

--- back/server.js
+++ back/server.js
@@ -6,21 +6,21 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 
-import { connectDB } from "./lib/db.js"
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.use(express.json({limit: "10mb"}))
-app.use(cookieParser())
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
-app.use("/api/cart", cartRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
